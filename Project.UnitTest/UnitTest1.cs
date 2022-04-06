a6043b803ccf395c7f87c33e277997ded7b1a0e4
İsmail Gür <ismailgur.57@gmail.com>
2022-04-06 03:42:12 +0300

    unit test ok.

--- Project.UnitTest/UnitTest1.cs
+++ Project.UnitTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Project.Business.MovieIntegration;
 using System;
 using Xunit;
 
@@ -5,10 +6,18 @@ namespace Project.UnitTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 20)]
+        public void Test1(int page, int expected)
         {
+            // Bu testte api den dönen liste adedinin 20 olmasý bekleniyor.
 
+            MovieAPI api = new MovieAPI("https://api.themoviedb.org/3", "23818476598777d4bf155e9500fefb82");
+
+            var data = api.GetUpcomingList(page, out int totalResultCount);
+
+
+            Assert.Equal(expected, data.Count);
         }
     }
 }
